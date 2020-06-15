2ddc0cdd9eeaf6beef78d389f6cdde082b5ac340
Jamie Barton <jbarton10@email.arizona.edu>
2020-06-14 17:50:58 -0700

    added all pages and did work on resume page

--- logic.js
+++ logic.js
@@ -3,3 +3,10 @@ console.log("linked");
 $("#home").on("click", function () {});
 $("#resume").on("click", function () {});
 $("#portfolio").on("click", function () {});
+
+$("#linkedIn").on("click", function () {
+  window.open("https://www.linkedin.com/in/jamie-barton-202530181/");
+});
+$("#gitHub").on("click", function () {
+  window.open("https://github.com/jbarton10");
+});
