4af8759d62ea3f78ed5dddd5adf6b3355d0b7bad
Jamie Barton <jbarton10@email.arizona.edu>
2020-06-18 16:58:57 -0700

    Almost done need to do server and make minor adjustments

--- logic.js
+++ logic.js
@@ -4,10 +4,10 @@ $("#home").on("click", function () {});
 $("#resume").on("click", function () {});
 $("#portfolio").on("click", function () {});
 
-$("#linkedIn").on("click", function () {
+$(".linkedIn").on("click", function () {
   window.open("https://www.linkedin.com/in/jamie-barton-202530181/");
 });
-$("#gitHub").on("click", function () {
+$(".gitHub").on("click", function () {
   window.open("https://github.com/jbarton10");
 });
 
