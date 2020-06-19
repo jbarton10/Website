05c2b266a87fc7df8863f8a0457c59465e39bee7
Jamie Barton <jbarton10@email.arizona.edu>
2020-06-19 15:30:15 -0700

    General Server stuff up and routing working

---
+++ server.js
@@ -0,0 +1,42 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Route for home page
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+//Route for survey page
+app.get("/resume", function (req, res) {
+  res.sendFile(path.join(__dirname, "./resume.html"));
+});
+//Route for survey page
+app.get("/portfolio", function (req, res) {
+  res.sendFile(path.join(__dirname, "./portfolio.html"));
+});
+
+app.get("/style.css", function (req, res) {
+  res.sendFile(path.join(__dirname, "./style.css"));
+});
+app.get("/logic.js", function (req, res) {
+  res.sendFile(path.join(__dirname, "./logic.js"));
+});
+app.use("/assets", express.static(__dirname + "/assets"));
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
